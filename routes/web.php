70ff70bded66fe2d5bfd3229eabe5979d8e254a7
hannan84 <hannansarkar84@gmail.com>
2025-06-25 15:16:13 +0600

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExternalLoginController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/auth/external-login', [ExternalLoginController::class, 'handle']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/logout', [LoginController::class, 'logout']);
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
